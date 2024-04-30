b4289ed5b40f15ff9e14090b5a7103ce144f8c0e
Adarsh Dwivedi <adi3799adi@gmail.com>
2024-04-30 12:07:29 +0530

    Application Commented

--- src/pages/private/dashboard.jsx
+++ src/pages/private/dashboard.jsx
@@ -2,16 +2,14 @@ import uppclLogo from "./../../assets/logo.jpeg";
 function Dashboard() {
   return (
     <div className="min-h-full flex justify-center items-center">
-      <div className="text-center">
-        <div
-          className="block max-w-xl p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 
-        dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
-        >
-          <img className="mx-auto w-60" src={uppclLogo} alt="logo" />
-          <h1 className="text-3xl font-bold mt-4">
-            Feeder/Bay-Wise Energy Monitoring
-          </h1>
-        </div>
+      <div
+        className="block max-w-xl p-6 border border-gray-200 rounded-lg shadow bg-gray-100 
+        dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 w-full"
+      >
+        <img className="mx-auto w-60" src={uppclLogo} alt="logo" />
+        <h1 className="text-3xl font-bold mt-4 text-center">
+          New Verified Bill Portal
+        </h1>
       </div>
     </div>
   );
